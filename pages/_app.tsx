5b325cb8ed283dcc8636cff225c8aeb7869053f9
tuco2905 <136021160+tuco2905@users.noreply.github.com>
2025-02-18 18:23:59 -0300

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import Layout from '@/components/layout/Layout';
+import '@/styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <main className="h-full bg-white">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </main>
+    </SessionProvider>
+  );
+}
